09bd0ad1c80ce2968d5255838b287ebc440a5962
LCLP <lukas@lclpnet.work>
2025-10-12 19:11:03 +0200

    rename OrientedBlockPos to BlockFace

---
+++ gaco-math/src/main/java/work/lclpnet/gaco/math/BlockFace.java
@@ -0,0 +1,14 @@
+package work.lclpnet.gaco.math;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+public record BlockFace(BlockPos pos, Direction face) {
+
+    public static final Codec<BlockFace> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            BlockPos.CODEC.fieldOf("pos").forGetter(BlockFace::pos),
+            Direction.CODEC.fieldOf("face").forGetter(BlockFace::face)
+    ).apply(instance, BlockFace::new));
+}
